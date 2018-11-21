b00f0f396a4c683c39a55417820645d27ca15ad5
iashchuk <221box@list.ru>
2018-11-22 02:46:18 +0300

    Добавляет тест для управления жизнями игрока #2

--- js/logic/change-lives.test.js
+++ js/logic/change-lives.test.js
@@ -4,8 +4,15 @@ import {changeLives} from './change-lives';
 
 
 describe(`Change lives`, () => {
+
   it(`should return 2 if mistake answer and 3 lives`, () => {
     const current = Object.assign({}, START_GAME);
     assert.equal(changeLives(current, AnswerScore.WRONG).lives, 2);
   });
+
+  it(`should return 0 if mistake answer and 1 lives`, () => {
+    const current = Object.assign({}, START_GAME, {lives: 1});
+    assert.equal(changeLives(current, AnswerScore.WRONG).lives, 0);
+  });
+
 });
