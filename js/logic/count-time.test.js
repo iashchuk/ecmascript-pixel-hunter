5a15991b4ae7d5a4e77ec35e6a441a7e18e6a620
iashchuk <221box@list.ru>
2018-11-23 02:40:08 +0300

    Добавляет/переделывает тест для функции подсчета времени #3

--- js/logic/count-time.test.js
+++ js/logic/count-time.test.js
@@ -1,23 +1,25 @@
 import {assert} from 'chai';
-import {START_GAME} from './config';
+import {INITIAL_GAME} from './config';
 import {countTime} from './count-time';
 
 describe(`Count time`, () => {
 
-  it(`should set time on default timer`, () => {
-    assert.equal(countTime(START_GAME, START_GAME.time).time, 30);
+  it(`should return 29 after calling tick 1 time for start 30 sec`, () => {
+    const newTimer = countTime(INITIAL_GAME.time);
+    newTimer.tick();
+    assert.equal(newTimer.remainTime, 29);
   });
 
-  it(`should reset time and set value of 0`, () => {
-    assert.equal(countTime(START_GAME, 0).time, 0);
+  it(`should return Error if time is not a number`, () => {
+    assert.throws(() => countTime(`ten seconds`).time);
   });
 
   it(`should return Error if time is negative`, () => {
-    assert.throws(() => countTime(START_GAME, -10).time);
+    assert.throws(() => countTime(-10).time);
   });
 
   it(`should return Error if time is more max time`, () => {
-    assert.throws(() => countTime(START_GAME, 60).level);
+    assert.throws(() => countTime(60).time);
   });
 
 });
