c9bb8e9754319de4e7dbdc6ce3bcc98b7eba17a9
iashchuk <221box@list.ru>
2018-11-22 03:01:30 +0300

    Дорабатывает функцию управления жизнями игрока

--- js/logic/change-lives.js
+++ js/logic/change-lives.js
@@ -1,6 +1,11 @@
 const changeLives = (game, answer) => {
   const result = (answer) ? 0 : 1;
   const lives = game.lives - result;
+
+  if (lives < 0) {
+    throw new Error(`should not be negative value of lives`);
+  }
+
   return Object.assign({}, game, {lives});
 };
 
