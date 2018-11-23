72185e7de9b4e81359d35f00dabb53a7360a72a7
iashchuk <221box@list.ru>
2018-11-23 04:48:28 +0300

    Переделывает функцию подсчета очков

--- js/logic/scoring.js
+++ js/logic/scoring.js
@@ -1,15 +1,47 @@
-import {AnswerScore} from './config';
+import {AnswerScore, AnswerTime} from './config';
+
+const createAnswer = (answer, time) => {
+  return {
+    answer,
+    time
+  };
+};
+
 
 const scoring = (answers, lives) => {
   if (answers.length < 10) {
     return -1;
   }
 
+  if (typeof lives !== `number`) {
+    throw new Error(`lives should be of type number`);
+  }
+
   if (lives < 0) {
     throw new Error(`should not be negative value of lives`);
   }
 
-  return (answers.reduce((total, current) => total + current) + lives * AnswerScore.BONUS);
+  let score = 0;
+
+  answers.forEach((result) => {
+    if (result.time < 0) {
+      throw new Error(`should not be negative value of time`);
+    }
+
+    if (typeof result.answer !== `boolean` && result.answer !== 0 && result.answer !== 1) {
+      throw new Error(`answer should be of type boolean or 0/1`);
+    }
+
+    if (result.answer && result.time > AnswerTime.FAST) {
+      score += AnswerScore.FAST;
+    } else if (result.answer && result.time < AnswerTime.SLOW) {
+      score += AnswerScore.SLOW;
+    } else if (result.answer) {
+      score += AnswerScore.NORMAL;
+    }
+  });
+  score += lives * AnswerScore.BONUS;
+  return score;
 };
 
-export {scoring};
+export {createAnswer, scoring};
