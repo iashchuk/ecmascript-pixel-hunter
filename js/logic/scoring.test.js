9436abfbd1a9c990053e1a4f38db444f71c6e1f3
iashchuk <221box@list.ru>
2018-11-23 04:50:33 +0300

    Переделывает тесты подсчета очков

--- js/logic/scoring.test.js
+++ js/logic/scoring.test.js
@@ -1,64 +1,62 @@
 import {assert} from 'chai';
-import {AnswerScore} from './config';
-import {scoring} from './scoring';
+import {scoring, createAnswer} from './scoring';
 
 describe(`Scoring`, () => {
   it(`should return -1 if less than 10 answers`, () => {
     assert.equal(scoring([
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.WRONG,
-      AnswerScore.WRONG,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL
+      createAnswer(true, 25),
+      createAnswer(true, 25),
+      createAnswer(false, 15),
+      createAnswer(false, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
     ], 3), -1);
   });
 
   it(`should return 1150 if all lives and normal speed`, () => {
     assert.equal(scoring([
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL
+      createAnswer(true, 19),
+      createAnswer(true, 18),
+      createAnswer(true, 17),
+      createAnswer(true, 16),
+      createAnswer(true, 15),
+      createAnswer(true, 14),
+      createAnswer(true, 13),
+      createAnswer(true, 12),
+      createAnswer(true, 11),
+      createAnswer(true, 10),
     ], 3), 1150);
   });
 
   it(`should return Error if lives is negative`, () => {
     assert.throws(() => scoring([
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL,
-      AnswerScore.NORMAL
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
+      createAnswer(true, 15),
     ], -1));
   });
 
   it(`should return 1650 for maximum score`, () => {
     assert.equal(scoring([
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST,
-      AnswerScore.FAST
+      createAnswer(true, 30),
+      createAnswer(true, 29),
+      createAnswer(true, 28),
+      createAnswer(true, 27),
+      createAnswer(true, 26),
+      createAnswer(true, 25),
+      createAnswer(true, 24),
+      createAnswer(true, 23),
+      createAnswer(true, 22),
+      createAnswer(true, 21),
     ], 3), 1650);
   });
 });
