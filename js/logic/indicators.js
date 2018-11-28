60cace3e361c6e775620f9ec0f010a3eadaa8fde
iashchuk <221box@list.ru>
2018-11-28 23:07:57 +0300

    Добавляет функцию создания индикатора ответа

---
+++ js/logic/indicators.js
@@ -0,0 +1,25 @@
+import indicatorsTemplate from '../templates/indicators.template';
+
+
+const getIndicators = (answers) => {
+  const indicatorsBlock = indicatorsTemplate();
+  const indicators = indicatorsBlock.querySelectorAll(`li`);
+
+  if (answers.length > 0) {
+    answers.forEach((answer, index) => {
+      indicators[index].classList.remove(`stats__result--unknown`);
+      if (!answer.isCorrect) {
+        indicators[index].classList.add(`stats__result--wrong`);
+      } else if (answer.isCorrect && answer.isFast) {
+        indicators[index].classList.add(`stats__result--fast`);
+      } else if (answer.isCorrect && answer.isSlow) {
+        indicators[index].classList.add(`stats__result--slow`);
+      } else if (answer.isCorrect) {
+        indicators[index].classList.add(`stats__result--correct`);
+      }
+    });
+  }
+  return indicatorsBlock;
+};
+
+export {getIndicators};
