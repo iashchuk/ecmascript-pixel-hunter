977723aa8cf754aff57bccd980e25a2de1eb6026
iashchuk <221box@list.ru>
2018-11-28 17:17:11 +0300

    Переделывает модуль с шаблоном правил игры

---
+++ js/templates/rules.js
@@ -0,0 +1,48 @@
+import {getElementFromTemplate, changeScreen, createScreen} from '../services';
+import {getBackButton} from './header';
+import {getDefaultState, getGameScreen} from '../logic/game';
+import footer from './footer';
+import {INITIAL_GAME} from '../logic/config';
+
+
+const rulesTemplate = `
+  <section class="rules">
+    <h2 class="rules__title">Правила</h2>
+    <ul class="rules__description">
+      <li>Угадай 10 раз для каждого изображения фото
+        <img class="rules__icon" src="img/icon-photo.png" width="32" height="31" alt="Фото"> или рисунок
+        <img class="rules__icon" src="img/icon-paint.png" width="32" height="31" alt="Рисунок"></li>
+      <li>Фотографиями или рисунками могут быть оба изображения.</li>
+      <li>На каждую попытку отводится 30 секунд.</li>
+      <li>Ошибиться можно не более 3 раз.</li>
+    </ul>
+    <p class="rules__ready">Готовы?</p>
+    <form class="rules__form">
+      <input class="rules__input" type="text" placeholder="Ваше Имя">
+      <button class="rules__button  continue" type="submit" disabled>Go!</button>
+    </form>
+  </section>
+`;
+
+
+export default () => {
+
+  const rulesElement = getElementFromTemplate(rulesTemplate);
+
+  const rulesInput = rulesElement.querySelector(`.rules__input`);
+  const rulesButton = rulesElement.querySelector(`.rules__button`);
+
+  const rulesInputHandler = () => {
+    rulesButton.disabled = !rulesInput.value;
+  };
+
+  const rulesButtonHandler = () => {
+    getDefaultState();
+    changeScreen(getGameScreen(INITIAL_GAME));
+  };
+
+  rulesInput.addEventListener(`input`, rulesInputHandler);
+  rulesButton.addEventListener(`click`, rulesButtonHandler);
+
+  return createScreen(getBackButton(), rulesElement, footer());
+};
