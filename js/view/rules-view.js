42c1ee2bfc41d7d91111154d7ecb736ab3e3e4d8
iashchuk <221box@list.ru>
2018-12-04 21:44:00 +0300

    Создает класс RulesView

---
+++ js/view/rules-view.js
@@ -0,0 +1,48 @@
+import AbstractView from './abstract-view';
+import back from '../templates/back-template';
+import footer from '../templates/footer';
+
+
+export default class RulesView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `
+      <header claass="header">${back}</header>
+      <section class="rules">
+        <h2 class="rules__title">Правила</h2>
+        <ul class="rules__description">
+          <li>Угадай 10 раз для каждого изображения фото
+            <img class="rules__icon" src="img/icon-photo.png" width="32" height="31" alt="Фото"> или рисунок
+            <img class="rules__icon" src="img/icon-paint.png" width="32" height="31" alt="Рисунок"></li>
+          <li>Фотографиями или рисунками могут быть оба изображения.</li>
+          <li>На каждую попытку отводится 30 секунд.</li>
+          <li>Ошибиться можно не более 3 раз.</li>
+        </ul>
+        <p class="rules__ready">Готовы?</p>
+        <form class="rules__form">
+          <input class="rules__input" type="text" placeholder="Ваше Имя">
+          <button class="rules__button  continue" type="submit" disabled>Go!</button>
+        </form>
+      </section>
+      ${footer}
+    `;
+  }
+
+  bind() {
+    const rulesButton = this.element.querySelector(`.rules__button`);
+    const rulesInput = this.element.querySelector(`.rules__input`);
+    const backButton = this.element.querySelector(`.back`);
+
+    const rulesInputHandler = () => (rulesButton.disabled = !rulesInput.value);
+
+    rulesInput.addEventListener(`input`, rulesInputHandler);
+    rulesButton.addEventListener(`click`, this.rulesButtonHandler);
+    backButton.addEventListener(`click`, this.backButtonHadler);
+  }
+
+  rulesButtonHandler() {}
+  backButtonHadler() {}
+}
