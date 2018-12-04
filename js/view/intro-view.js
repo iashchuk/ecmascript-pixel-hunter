a4cdebf0311bd2fa48ba23babd0f16a68e4cb857
iashchuk <221box@list.ru>
2018-12-04 21:08:38 +0300

    Создает класс IntroView

---
+++ js/view/intro-view.js
@@ -0,0 +1,27 @@
+import AbstractView from './abstract-view';
+import footer from '../templates/footer';
+
+
+export default class IntroView extends AbstractView {
+
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `
+    <section class="intro">
+      <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    </section>
+    ${footer}
+    `;
+  }
+
+  onAsteriskButtonClick() {}
+
+  bind() {
+    const asteriskButton = this.element.querySelector(`.intro__asterisk`);
+    asteriskButton.addEventListener(`click`, this.onAsteriskButtonClick);
+  }
+}
