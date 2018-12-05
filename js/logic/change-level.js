b8ecdfdd6d2f0ce36595b09d189ec0b68d2545f9
iashchuk <221box@list.ru>
2018-12-06 02:15:46 +0300

    Создает класс StatsView

--- js/logic/change-level.js
+++ js/logic/change-level.js
@@ -1,10 +1,14 @@
 import {MAX_GAME_VALUES} from './config';
 import {changeScreen} from '../services';
-import {getGameScreen} from './game';
-import stats from '../templates/stats';
-
+import {getGameScreen} from '../screens/game-screen';
+import stats from '../screens/stats-screen';
+import {getRandomInRange} from '../services';
+import {getGameState} from '../screens/game-screen';
 
 const changeLevel = (state) => {
+  state.level += 1;
+  state.time = getRandomInRange(0, 30);
+
   if (state.level < 0) {
     throw new Error(`should not be negative value of level`);
   }
@@ -13,12 +17,12 @@ const changeLevel = (state) => {
     throw new Error(`should not be more than max level`);
   }
 
-  if (state.lives === 0) {
+  if (!state.lives) {
     state.gameOver = true;
   }
 
   if (state.gameOver || (state.level === 10)) {
-    changeScreen(stats(state));
+    changeScreen(stats(getGameState()));
   } else {
     changeScreen(getGameScreen(state));
   }
