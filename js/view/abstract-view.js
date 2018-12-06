b810015716d8465bb967975894fc58a58050ca58
iashchuk <221box@list.ru>
2018-12-07 02:05:12 +0300

    Дорабатывает класс AbstractView

--- js/view/abstract-view.js
+++ js/view/abstract-view.js
@@ -1,21 +1,28 @@
-import {getElementFromTemplate} from '../services';
+import {render} from '../services';
 
 export default class AbstractView {
+
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`Can't instantiate AbstractView, only concrete one`);
+    }
+  }
 
   get template() {
     throw new Error(`You have to define template for view`);
   }
 
   get element() {
-    if (!this._element) {
-      this._element = this.render();
-      this.bind();
+    if (this._element) {
+      return this._element;
     }
+    this._element = this.render();
+    this.bind(this._element);
     return this._element;
   }
 
   render() {
-    return getElementFromTemplate(this.template);
+    return render(this.template);
   }
 
   bind() {}
