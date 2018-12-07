67964276982aa1bf650c1c44c2946017e336bce5
iashchuk <221box@list.ru>
2018-12-08 02:45:41 +0300

    Вносит изменения в класс StatsView

--- js/view/stats-view.js
+++ js/view/stats-view.js
@@ -1,8 +1,5 @@
 import AbstractView from './abstract-view';
-import back from './components/back-component';
-import footer from './components/footer-component';
 import indicators from './components/indicators-component';
-
 
 export default class StatsView extends AbstractView {
   constructor(state, scores) {
@@ -13,60 +10,49 @@ export default class StatsView extends AbstractView {
 
   get template() {
     return `
-    <header class="header">${back}</header>
-    <div class="result">
-      <h1>${!this.state.gameOver ? `Победа` : `Поражение`}</h1>
-      <table class="result__table">
-        <tr>
-          <td class="result__number">1.</td>
-          <td colspan="2" class="result__indicators">
-            ${indicators(this.state.answers)}
-          </td>
-          <td class="result__points">×&nbsp;100</td>
-          <td class="result__total">${this.normal.points}
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td class="result__extra">Бонус за скорость:</td>
-          <td class="result__extra">${this.fast.count}
-          &nbsp;<span class="stats__result stats__result--fast"></span></td>
-          <td class="result__points">×&nbsp;50</td>
-          <td class="result__total">${this.fast.points}</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td class="result__extra">Бонус за жизни:</td>
-          <td class="result__extra">${this.lives.count}
-          &nbsp;<span class="stats__result stats__result--alive"></span></td>
-          <td class="result__points">×&nbsp;50</td>
-          <td class="result__total">${this.lives.points}</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td class="result__extra">Штраф за медлительность:</td>
-          <td class="result__extra">${this.slow.count}
-          &nbsp;<span class="stats__result stats__result--slow"></span></td>
-          <td class="result__points">×&nbsp;50</td>
-          <td class="result__total">${this.slow.points}</td>
-        </tr>
-        <tr>
-          <td colspan="5" class="result__total  result__total--final">${this.total}
-          </td>
-        </tr>
-      </table>
-    </div>
-    ${footer}
+      <div class="result">
+        <h1>${!this.state.gameOver ? `Победа` : `Поражение`}</h1>
+        <table class="result__table">
+          <tr>
+            <td class="result__number">1.</td>
+            <td colspan="2" class="result__indicators">
+              ${indicators(this.state.answers)}
+            </td>
+            <td class="result__points">×&nbsp;100</td>
+            <td class="result__total">${this.normal.points}
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за скорость:</td>
+            <td class="result__extra">${this.fast.count}
+            &nbsp;<span class="stats__result stats__result--fast"></span></td>
+            <td class="result__points">×&nbsp;50</td>
+            <td class="result__total">${this.fast.points}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за жизни:</td>
+            <td class="result__extra">${this.lives.count}
+            &nbsp;<span class="stats__result stats__result--alive"></span></td>
+            <td class="result__points">×&nbsp;50</td>
+            <td class="result__total">${this.lives.points}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Штраф за медлительность:</td>
+            <td class="result__extra">${this.slow.count}
+            &nbsp;<span class="stats__result stats__result--slow"></span></td>
+            <td class="result__points">×&nbsp;50</td>
+            <td class="result__total">${this.slow.points}</td>
+          </tr>
+          <tr>
+            <td colspan="5" class="result__total  result__total--final">${this.total}
+            </td>
+          </tr>
+        </table>
+      </div>
     `;
-  }
-
-  backButtonHadler() {}
-
-  bind() {
-    const backButton = this.element.querySelector(`.back`);
-    backButton.addEventListener(`click`, () => {
-      this.backButtonHadler();
-    });
   }
 }
 
