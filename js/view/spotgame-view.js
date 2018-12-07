10e1523863fca89cd794e58d10bd5011c448b047
iashchuk <221box@list.ru>
2018-12-08 02:37:24 +0300

    Вносит изменения в класс SpotGameView

--- js/view/spotgame-view.js
+++ js/view/spotgame-view.js
@@ -1,6 +1,4 @@
 import AbstractView from './abstract-view';
-import HeaderView from './header-view';
-import footer from './components/footer-component';
 import indicators from './components/indicators-component';
 
 export default class SpotGameView extends AbstractView {
@@ -13,28 +11,24 @@ export default class SpotGameView extends AbstractView {
   }
 
   get template() {
-    const header = new HeaderView(this.state);
-
     return `
-    ${header.template}
-    <section class="game">
-      <p class="game__task">${this.game.description}</p>
-      <form class="game__content game__content--wide">
-        <div class="game__option" data-type="${this.params.type}" data-number="${this.params.index}">
-          <img src="${this.params.src}" alt="Option ${this.params.index}" width="705" height="455">
-          <label class="game__answer  game__answer--photo">
-            <input class="visually-hidden" name="question${this.params.index}" type="radio" value="photo">
-            <span>Фото</span>
-          </label>
-          <label class="game__answer  game__answer--wide  game__answer--paint">
-            <input class="visually-hidden" name="question${this.params.index}" type="radio" value="paint">
-            <span>Рисунок</span>
-          </label>
-        </div>
-      </form>
-      ${indicators(this.state.answers)}
-    </section>
-    ${footer}
+      <section class="game">
+        <p class="game__task">${this.game.description}</p>
+        <form class="game__content game__content--wide">
+          <div class="game__option" data-type="${this.params.type}" data-number="${this.params.index}">
+            <img src="${this.params.src}" alt="Option ${this.params.index}" width="705" height="455">
+            <label class="game__answer  game__answer--photo">
+              <input class="visually-hidden" name="question${this.params.index}" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer  game__answer--wide  game__answer--paint">
+              <input class="visually-hidden" name="question${this.params.index}" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
+          </div>
+        </form>
+        ${indicators(this.state.answers)}
+      </section>
     `;
   }
 
@@ -42,18 +36,14 @@ export default class SpotGameView extends AbstractView {
   bind() {
     const gameContent = this.element.querySelector(`.game__content`);
     const gameOption = this.element.querySelector(`.game__option`).dataset.type;
-    const backButton = this.element.querySelector(`.back`);
 
     gameContent.addEventListener(`click`, () => {
-      let result = (gameOption === gameContent.question1.value);
+      const answer = (gameOption === gameContent.question1.value);
       if (gameContent.question1.value) {
-        this.answerHandler(result);
+        this.answerHandler(answer);
       }
     });
-
-    backButton.addEventListener(`click`, () => this.backButtonHandler());
   }
 
-  backButtonHandler() {}
   answerHandler() {}
 }
