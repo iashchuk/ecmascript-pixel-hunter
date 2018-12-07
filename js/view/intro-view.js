8720de644457f833478efe4ce5ddb2df00535670
iashchuk <221box@list.ru>
2018-12-08 02:26:04 +0300

    Вносит изменения в класс IntroView

--- js/view/intro-view.js
+++ js/view/intro-view.js
@@ -1,5 +1,4 @@
 import AbstractView from './abstract-view';
-import footer from './components/footer-component';
 
 
 export default class IntroView extends AbstractView {
@@ -10,17 +9,16 @@ export default class IntroView extends AbstractView {
 
   get template() {
     return `
-    <section class="intro">
-      <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
-      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
-    </section>
-    ${footer}
+      <section class="intro">
+        <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+        <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+      </section>
     `;
   }
 
   bind() {
     const asteriskButton = this.element.querySelector(`.intro__asterisk`);
-    asteriskButton.addEventListener(`click`, this.asteriskButtonHandler);
+    asteriskButton.addEventListener(`click`, () => this.asteriskButtonHandler());
   }
 
   asteriskButtonHandler() {}
