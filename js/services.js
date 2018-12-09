ad2bca9edf4b20aca81651e70622b10e55d35785
iashchuk <221box@list.ru>
2018-12-10 01:13:09 +0300

    Оптимизирует сервисный модуль

--- js/services.js
+++ js/services.js
@@ -1,25 +1,3 @@
-const mainElement = document.querySelector(`#main`);
-
-const changeScreen = (element) => {
-  mainElement.innerHTML = ``;
-  mainElement.appendChild(element);
-};
-
-
-const render = (template) => {
-  const wrapperElement = document.createElement(`div`);
-  wrapperElement.innerHTML = template.trim();
-  return wrapperElement;
-};
-
-const createScreen = (...elements) => {
-  const fragment = document.createDocumentFragment();
-  elements.forEach((element) => {
-    fragment.appendChild(element);
-  });
-  return fragment;
-};
-
 const getRandomElement = function (arrayElements) {
   let index = Math.floor(Math.random() * arrayElements.length);
   return arrayElements[index];
@@ -29,4 +7,4 @@ const getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-export {changeScreen, render, createScreen, getRandomElement, getRandomInRange};
+export {getRandomElement, getRandomInRange};
