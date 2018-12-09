211d63db03219550b4b555c49f9cc8ff0a412925
iashchuk <221box@list.ru>
2018-12-09 06:57:34 +0300

    Создает класс RulesController

---
+++ js/controllers/rules-controller.js
@@ -0,0 +1,28 @@
+import RulesView from '../view/rules-view';
+import BackView from '../view/back-view';
+import FooterView from '../view/footer-view';
+import Controller from './controller';
+
+
+export default class RulesController extends Controller {
+  constructor() {
+    super();
+    this.back = new BackView();
+    this.content = new RulesView();
+    this.footer = new FooterView();
+
+    this.root = document.createElement(`div`);
+    this.root.appendChild(this.back.element);
+    this.root.appendChild(this.content.element);
+    this.root.appendChild(this.footer.element);
+  }
+
+  showGreeting() {}
+  changeView() {}
+
+  init() {
+    this.back.backButtonHandler = this.showGreeting;
+    this.content.rulesButtonHandler = this.changeView;
+    this.render(this.root);
+  }
+}
