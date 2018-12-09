5c75b60cc1612a63a9ccaca795f7a771997fe87b
iashchuk <221box@list.ru>
2018-12-09 08:27:16 +0300

    Дорабатывает Lives Component

--- js/views/components/lives-component.js
+++ js/views/components/lives-component.js
@@ -1,8 +1,12 @@
+const drawHeart = (full) => {
+  return `<img src="img/heart__${full ? `full` : `empty`}.svg" class="game__heart" alt="Life" width="31" height="27">`;
+};
+
 export default (lives) => {
   return `
     <div class="game__lives">
-        <img src="img/heart__${lives >= 1 ? `full` : `empty`}.svg" class="game__heart" alt="Life" width="31" height="27">
-        <img src="img/heart__${lives >= 2 ? `full` : `empty`}.svg" class="game__heart" alt="Life" width="31" height="27">
-        <img src="img/heart__${lives >= 3 ? `full` : `empty`}.svg" class="game__heart" alt="Life" width="31" height="27">
+        ${drawHeart(lives >= 1)}
+        ${drawHeart(lives >= 2)}
+        ${drawHeart(lives >= 3)}
     </div>`;
 };
