bee99b7b67c6fa961fef36c809ef762625bcd6f7
iashchuk <221box@list.ru>
2018-12-12 03:31:22 +0300

    Добавляет класс ModalErrorView

---
+++ js/views/modal-error-view.js
@@ -0,0 +1,19 @@
+import AbstractView from './abstract-view';
+
+
+export default class ModalErrorView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `
+      <section class="modal">
+        <div class="modal__inner">
+          <h2 class="modal__title">Произошла ошибка!</h2>
+          <p class="modal__text modal__text--error">Статус: 404. Пожалуйста, перезагрузите страницу.</p>
+        </div>
+      </section>
+    `;
+  }
+}
