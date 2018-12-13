38347557f672194c81e76fb18b9bfb6dc1340b41
iashchuk <221box@list.ru>
2018-12-13 05:22:39 +0300

    Дорабатывает класс Loader (load/save-results)

--- js/data/loader.js
+++ js/data/loader.js
@@ -1,20 +1,49 @@
 import adaptServerData from './adapter';
 
-const DOWNLOAD_URL = `https://es.dump.academy/pixel-hunter/questions`;
+const SERVER_URL = `https://es.dump.academy/pixel-hunter`;
+const DEFAULT_NAME = `Marty McFly JS`;
+const APP_ID = 12122018;
+
+const StatusCode = {
+  SUCCESS: 200,
+  REDIRECTION: 300
+};
 
 const checkStatus = (response) => {
-  if (response.status >= 200 && response.status < 300) {
+  if (response.status >= StatusCode.SUCCESS && response.status < StatusCode.REDIRECTION) {
     return response;
   } else {
-    throw new Error(`${response.status}: ${response.statusText}`);
+    throw new Error(`${response.status}: ${response.statusText} ха-ха`);
   }
 };
 
+const toJSON = (response) => response.json();
+
 export default class Loader {
+
   static loadData() {
-    return fetch(DOWNLOAD_URL)
+    return fetch(`${SERVER_URL}/questions`)
       .then(checkStatus)
-      .then((response) => response.json())
+      .then(toJSON)
       .then((data) => adaptServerData(data));
+  }
+
+  static loadResults(name = DEFAULT_NAME) {
+    return fetch(`${SERVER_URL}/stats/${APP_ID}-${name}`)
+      .then(checkStatus)
+      .then(toJSON);
+  }
+
+  static saveResults(data, name = DEFAULT_NAME) {
+    data = Object.assign({name}, data);
+    const requestSettings = {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': `application/json`
+      },
+      method: `POST`
+    };
+    return fetch(`${SERVER_URL}/stats/${APP_ID}-${name}`, requestSettings)
+      .then(checkStatus);
   }
 }
