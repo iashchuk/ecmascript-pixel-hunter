ff317d5f0e561db447143249facd248c2ff17e08
iashchuk <221box@list.ru>
2018-12-13 05:34:34 +0300

    Дорабатывает класс Router

--- js/router.js
+++ js/router.js
@@ -28,20 +28,25 @@ export default class Router {
   static showRules() {
     const rules = new RulesController();
     rules.showGreeting = () => this.showGreeting();
-    rules.changeView = () => this.showGame();
+    rules.changeView = (player) => this.showGame(player);
     rules.init();
   }
 
-  static showStats(state) {
-    const stats = new StatsController(state);
-    stats.showGreeting = () => this.showGreeting();
-    stats.init();
+  static showStats(results, player) {
+    Loader.saveResults(results, player)
+      .then(() => Loader.loadResults(player))
+      .then((data) => {
+        const stats = new StatsController(data);
+        stats.showGreeting = () => this.showGreeting();
+        stats.init();
+      })
+      .catch(this.showModalError);
   }
 
-  static showGame() {
-    const model = new GameModel(this.games);
+  static showGame(player) {
+    const model = new GameModel(this.games, player);
     const game = new GameController(model);
-    game.changeView = () => this.showStats(model.state);
+    game.changeView = (results) => this.showStats(results, player);
     game.showModalConfirm = () => this.showModalConfirm();
     game.init();
   }
