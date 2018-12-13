b1d887fca8390b9a55ab302c06db3a44b123b050
iashchuk <221box@list.ru>
2018-12-13 05:33:56 +0300

    Дорабатывает классы StatsView/StatsController

--- js/views/stats-view.js
+++ js/views/stats-view.js
@@ -1,60 +1,62 @@
 import AbstractView from './abstract-view';
 import indicators from './components/indicators-component';
-import scoring from '../logic/scoring';
-
 
 export default class StatsView extends AbstractView {
-  constructor(state) {
+  constructor(results) {
     super();
-    this.state = state;
-    ({normal: this.normal, fast: this.fast, slow: this.slow, lives: this.lives, total: this.total} = scoring(this.state.answers, this.state.lives));
+    this.results = results.reverse();
   }
 
   get template() {
-    return `
-      <div class="result">
-        <h1>${this.state.lives ? `Победа` : `Поражение`}</h1>
+    const getResultTable = (result, index) => {
+      return `
+        <h1>${result.lives.count ? `Победа` : `Поражение`}</h1>
         <table class="result__table">
           <tr>
-            <td class="result__number">1.</td>
+            <td class="result__number">${index + 1}.</td>
             <td colspan="2" class="result__indicators">
-              ${indicators(this.state.answers)}
+              ${indicators(result.answers)}
             </td>
             <td class="result__points">×&nbsp;100</td>
-            <td class="result__total">${this.normal.points}
+            <td class="result__total">${result.normal.points}
             </td>
           </tr>
           <tr>
             <td></td>
             <td class="result__extra">Бонус за скорость:</td>
-            <td class="result__extra">${this.fast.count}
+            <td class="result__extra">${result.fast.count}
             &nbsp;<span class="stats__result stats__result--fast"></span></td>
             <td class="result__points">×&nbsp;50</td>
-            <td class="result__total">${this.fast.points}</td>
+            <td class="result__total">${result.fast.points}</td>
           </tr>
           <tr>
             <td></td>
             <td class="result__extra">Бонус за жизни:</td>
-            <td class="result__extra">${this.lives.count}
+            <td class="result__extra">${result.lives.count}
             &nbsp;<span class="stats__result stats__result--alive"></span></td>
             <td class="result__points">×&nbsp;50</td>
-            <td class="result__total">${this.lives.points}</td>
+            <td class="result__total">${result.lives.points}</td>
           </tr>
           <tr>
             <td></td>
             <td class="result__extra">Штраф за медлительность:</td>
-            <td class="result__extra">${this.slow.count}
+            <td class="result__extra">${result.slow.count}
             &nbsp;<span class="stats__result stats__result--slow"></span></td>
             <td class="result__points">×&nbsp;50</td>
-            <td class="result__total">${this.slow.points}</td>
+            <td class="result__total">${result.slow.points}</td>
           </tr>
           <tr>
-            <td colspan="5" class="result__total  result__total--final">${this.total}
+            <td colspan="5" class="result__total  result__total--final">${result.lives.count ? result.total : `FAIL`}
             </td>
           </tr>
         </table>
+    `;
+    };
+
+    return `
+      <div class="result">
+        ${this.results.map((result, index) => (getResultTable(result, index))).join(``)}
       </div>
     `;
   }
 }
-
