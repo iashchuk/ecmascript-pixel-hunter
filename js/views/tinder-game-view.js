eacc81bd7adf6f8f8aa10d72061206772302eccd
iashchuk <221box@list.ru>
2018-12-19 09:42:23 +0300

    Прелоад изображений. Дорабатывает класс TinderGameView

--- js/views/tinder-game-view.js
+++ js/views/tinder-game-view.js
@@ -1,6 +1,6 @@
 import AbstractView from './abstract-view';
 import indicators from './components/indicators-component';
-import {resizeImages} from '../data/resize';
+import resize from '../data/resize';
 import {DEBUG} from '../logic/config';
 
 
@@ -13,6 +13,35 @@ export default class TinderGameView extends AbstractView {
     ([this.params] = this.game.params);
   }
 
+  get images() {
+    const images = [];
+    this.game.params.map((param) => {
+      const image = new Image();
+      image.src = param.src;
+
+      const frame = {
+        width: param.width,
+        height: param.height
+      };
+
+      const given = {
+        width: image.naturalWidth,
+        height: image.naturalHeight
+      };
+
+      const newSize = resize(frame, given);
+
+      image.type = param.type;
+      image.index = param.index;
+      image.width = newSize.width;
+      image.height = newSize.height;
+
+      images.push(image);
+
+    });
+    return images;
+  }
+
   get template() {
     return `
       <section class="game">
@@ -20,7 +49,7 @@ export default class TinderGameView extends AbstractView {
         <form class="game__content game__content--wide">
           <div class="game__option" data-type="${this.params.type}" data-number="${this.params.index}">
             ${DEBUG ? `<span class="debug">${this.params.type}</span>` : ``};
-            <img src="${this.params.src}" alt="Option ${this.params.index}" width="705" height="455">
+            <img src="${this.images[0].src}" alt="Option ${this.images[0].index}" width="${this.images[0].width}" height="${this.images[0].height}">
             <label class="game__answer  game__answer--photo">
               <input class="visually-hidden" name="question${this.params.index}" type="radio" value="photo">
               <span>Фото</span>
@@ -41,7 +70,6 @@ export default class TinderGameView extends AbstractView {
     const gameContent = this.element.querySelector(`.game__content`);
     const gameOption = this.element.querySelector(`.game__option`).dataset.type;
 
-    resizeImages(gameContent);
     gameContent.addEventListener(`click`, () => {
       const answer = (gameOption === gameContent.question1.value);
       if (gameContent.question1.value) {
