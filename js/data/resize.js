e85428bacce9eb539d54fbc1cab997b46a10a3c4
iashchuk <221box@list.ru>
2018-12-19 08:05:35 +0300

    Финальная проверка. Исправляет замечания по Д19. Дорабатывает код

--- js/data/resize.js
+++ js/data/resize.js
@@ -1,4 +1,4 @@
-const resize = (frame, given) => {
+export default (frame, given) => {
   const widthRatio = given.width / frame.width;
   const heightRatio = given.height / frame.height;
   const ratio = Math.max(widthRatio, heightRatio);
@@ -8,24 +8,3 @@ const resize = (frame, given) => {
     height: Math.floor(given.height / ratio),
   };
 };
-
-
-const resizeImages = (content) => {
-  content.querySelectorAll(`img`).forEach((img) => {
-    img.addEventListener(`load`, () => {
-      const frame = {
-        width: img.parentElement.clientWidth,
-        height: img.parentElement.clientHeight
-      };
-      const given = {
-        width: img.naturalWidth,
-        height: img.naturalHeight
-      };
-      const resizeImage = resize(frame, given);
-      img.style.width = resizeImage.width + `px`;
-      img.style.height = resizeImage.height + `px`;
-    });
-  });
-};
-
-export {resize, resizeImages};
